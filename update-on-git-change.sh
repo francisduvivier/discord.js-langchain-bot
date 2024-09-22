fb8aacba758a4fa724e3f374487903b65e145b7f
Francis Duvivier <francisduvivier@users.noreply.github.com>
2024-09-22 18:14:28 +0200

    Delete update-on-git-change.sh

--- update-on-git-change.sh
+++
@@ -1,7 +0,0 @@
-source /home/fduvivier/.bash_profile
-SCRIPT_DIR=$(dirname $0)
-cd $SCRIPT_DIR
-ssh fduvivier@192.168.0.55 "hi there from master"
-
-git stash -m 'local stuff'
-git fetch && git checkout todel/keep-bot-alive
